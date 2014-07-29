a6b5adbc806878396686e428f48621c7413aa248
Julien Le Glaunec <jleglaunec@gmail.com>
2014-07-29 08:50:15 +0200

    development mail configuration

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,16 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :user_name => '22609eedf16a6ca7f',
+    :password => 'aadabda9c0a46d',
+    :address => 'mailtrap.io',
+    :domain => 'mailtrap.io',
+    :port => '2525',
+    :authentication => :cram_md5,
+    :enable_starttls_auto => true
+  }
+  
 end
