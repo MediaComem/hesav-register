b652fa6e33ff93ab62520cdb673948ab290777ef
Julien Le Glaunec <jleglaunec@gmail.com>
2014-08-20 09:08:16 +0200

    create the root view of the platform

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
+  root 'application#index'
 
   # GOUVEOLE
   get '/gouveole_registrations' => 'gouveole_registrations#new'
