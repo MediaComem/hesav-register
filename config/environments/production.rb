18b5f8c44f7e85c261ece20af96f56cc7956b9c3
Adrien Bigler <adrienbigler@u44pc02.einet.ad.eivd.ch>
2014-08-21 15:18:43 +0200

    Setup smtp config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,4 +81,10 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.heig-vd.ch',
+    enable_starttls_auto: false
+  }
+
 end
