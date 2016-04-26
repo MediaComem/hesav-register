88a139702245604714c1e70fc27d6c3c589294d9
Adrien Bigler <adrien.bigler@heig-vd.ch>
2016-04-26 10:40:45 +0200

    Psy 16 form

--- app/models/psy16_registration.rb
+++ app/models/psy16_registration.rb
@@ -1,4 +1,8 @@
 class Psy16Registration < ActiveRecord::Base
+	before_save :default_values
+	validates :last_name,:first_name,:employer,:job,:email,:shopID,:country,:npa,:city,:street,  presence: true
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	validates :email, format: { with: VALID_EMAIL_REGEX }
 	store_accessor :registration_type, [
     :type_morning,
     :type_afternoon,
