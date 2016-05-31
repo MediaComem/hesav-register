1f1b0ec6558700008b6257134fdfdac14e8dab5e
Mathias <mathias.oberson@heig-vd.ch>
2016-05-31 15:40:55 +0200

    Updated js16 csv service to match the js16 model attributes

---
+++ app/models/js16_registration.rb
@@ -0,0 +1,17 @@
+class Js16Registration < ActiveRecord::Base
+	before_save :default_values
+	validates :last_name, :first_name, :employer, :email, :shopID, :npa, :city, :street, presence: true
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	validates :email, format: {with: VALID_EMAIL_REGEX}
+	store_accessor :registration_type, [
+		:type_price,
+		:type_choice_1,
+		:type_choice_2
+	]
+	belongs_to :event
+
+	def default_values
+		self.payed ||= false
+		true
+	end
+end
