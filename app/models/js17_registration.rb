02908919fd7a04b830da1110e370b1bb2ca2e8d2
camel113 <adrienbigler@gmail.com>
2017-07-11 16:10:23 +0200

    Fix bug hash syntax

--- app/models/js17_registration.rb
+++ app/models/js17_registration.rb
@@ -13,6 +13,12 @@ class Js17Registration < ActiveRecord::Base
     true
   end
   def self.ateliers
-    [{"name":"ATELIER 1","slug":"atelier1","descr":"Système de synchronisation de signaux physiologiques et biomécaniques en cyclisme : contrôle des paramètres de santé"},{"name":"ATELIER 2","slug":"atelier2","descr":"Entraînement sur vélo : de l’endurance continue aux sprints répétés","more":"(Tenue de sport nécessaire, atelier limité à 16 personnes)"},{"name":"ATELIER 3","slug":"atelier3","descr":"Dynamique de l'échographie du membre supérieur","more":"(Atelier pratique, limité à 15 personnes)"},{"name":"ATELIER 4","slug":"atelier4","descr":"Activité physique et cancer chez l’enfant : PASTEC pour avoir la pêche !"},{"name":"ATELIER 5","slug":"atelier5","descr":"Promotion du mouvement chez la femme enceinte, programme cantonal « ça marche »"},{"name":"ATELIER 6","slug":"atelier6","descr":"Je marche plus d'une heure par jour, et je m'en porte très bien, merci!"}]
+    [
+      {:name=>"ATELIER 1",:slug=>"atelier1",:descr=>"Système de synchronisation de signaux physiologiques et biomécaniques en cyclisme : contrôle des paramètres de santé"},
+      {:name=>"ATELIER 2",:slug=>"atelier2",:descr=>"Entraînement sur vélo : de l’endurance continue aux sprints répétés",:more=>"(Tenue de sport nécessaire, atelier limité à 16 personnes)"},
+      {:name=>"ATELIER 3",:slug=>"atelier3",:descr=>"Dynamique de l'échographie du membre supérieur",:more=>"(Atelier pratique, limité à 15 personnes)"},
+      {:name=>"ATELIER 4",:slug=>"atelier4",:descr=>"Activité physique et cancer chez l’enfant : PASTEC pour avoir la pêche !"},
+      {:name=>"ATELIER 5",:slug=>"atelier5",:descr=>"Promotion du mouvement chez la femme enceinte, programme cantonal « ça marche »"},
+      {:name=>"ATELIER 6",:slug=>"atelier6",:descr=>"Je marche plus d'une heure par jour, et je m'en porte très bien, merci!"}]
   end
 end
