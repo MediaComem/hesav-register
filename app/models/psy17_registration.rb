8c2e8db9303326405fdb578725a445f5fb7519af
camel113 <adrienbigler@gmail.com>
2017-07-12 11:45:31 +0200

    PSY17 Put back form validation

--- app/models/psy17_registration.rb
+++ app/models/psy17_registration.rb
@@ -1,6 +1,8 @@
 class Psy17Registration < ActiveRecord::Base
   before_save :default_values
-  
+  validates :last_name,:first_name,:employer,:job,:email,:shopID,:npa,:city,:street,:streetnumber,  presence: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, format: { with: VALID_EMAIL_REGEX }
   store_accessor :registration_type, [
     :type_price,
     :type_choice_am,
