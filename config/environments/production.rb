a4a414c14ec9358ce79f830971396d50e7cbacf9
camel113 <adrienbigler@gmail.com>
2017-11-28 15:33:30 +0100

    Gmail smtp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,11 +84,12 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :address              => "smtp.gmail.com",
-    :port                 => 587,
-    :user_name            => ENV['gmail_username'],
-    :password             => ENV['gmail_password'],
-    :authentication       => "plain",
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => "gmail.com",
+    :user_name => ENV['gmail_username'],
+    :password => ENV['gmail_password'],
+    :authentication => "plain",
     :enable_starttls_auto => true
   }
 
